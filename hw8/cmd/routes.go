0c0bed79d003e71551e2f3d61cec919c46bd37c6
sseibetsu <76789420+sseibetsu@users.noreply.github.com>
2024-05-14 13:21:01 +0500

    .

--- hw8/cmd/routes.go
+++ hw8/cmd/routes.go
@@ -16,17 +16,13 @@ func SetupRoutes(app *fiber.App) {
 		return
 	}
 
-	// Получение абсолютного пути к каталогу с исполняемым файлом
 	currentDir := filepath.Dir(executablePath)
 
 	fmt.Println("Текущий рабочий каталог в routes.go:", currentDir)
 
-	// Роуты для фактов (facts)
 	app.Get("/facts", handlers.ListFacts)
 	app.Get("/facts/:id", handlers.GetFact)
 	app.Post("/facts", handlers.CreateFact)
 	app.Put("/facts/:id", handlers.UpdateFact)
 	app.Delete("/facts/:id", handlers.DeleteFact)
-
-	// ... (ваш код для треков и плейлистов)
 }
